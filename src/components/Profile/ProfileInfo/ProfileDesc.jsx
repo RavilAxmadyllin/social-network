60e06fcfca17c259a066d14514033bbadad9dfce
RavilAxmadyllin <craftwaymiks@gmail.com>
2020-07-24 16:18:47 +0300

    add selector

---
+++ src/components/Profile/ProfileInfo/ProfileDesc.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './ProfileInfo.module.css'
+const ProfileDesc = ({profile, toggleProfileMode}) =>{
+    return(
+        <ul className={styles.descList}>
+            <Contact title={'aboutMe :'} value={profile.aboutMe}/>
+            <Contact title={'lookingForAJob :'} value={profile.lookingForAJob ? 'yeas' : 'no'}/>
+            <Contact title={'lookingForAJobDescription :'} value={profile.lookingForAJobDescription}/>
+            {Object.keys(profile.contacts).map( key =>{
+                return <Contact key={key} title={key} value={profile.contacts[key]} />
+            })}
+            <button onClick={()=> toggleProfileMode(true)}>change</button>
+        </ul>
+    )
+}
+const Contact = ({title, value}) =>{
+    return <li ><strong>{title} </strong> {value}</li>
+}
+
+export default ProfileDesc
---
+++ src/components/Profile/ProfileInfo/ProfileFormEdit.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {Input} from '../../FormComponent/FormComponent';
+import styles from './ProfileFormEdit.module.css'
+const ProfileForm = ({error, ...props}) => {
+    return (
+            <form onSubmit={props.handleSubmit}>
+                <button>sub</button> <button onClick={()=> props.toggle(false)}>back</button>
+                {error && <div style={{color:'red'}}>{error}</div>}
+                <div>Full name: <Field type={'text'} component={'input'} name={'fullName'}/></div>
+                <div>About me: <Field type={'text'} component={'input'} name={'aboutMe'}/></div>
+                <div>lookingForAJob : <Field type={'checkbox'} component={'input'} name={'lookingForAJob'}/></div>
+                <div>lookingForAJobDescription: <Field type={'text'} component={'textarea'} name={'lookingForAJobDescription'}/></div>
+                {Object.keys(props.profile.contacts).map( el =>{
+                    return <div key={el}>{el}<Field type={'text'} component={Input} name={`contacts.${el}`} /></div>
+                })}
+
+            </form>
+    )
+}
+
+const ProfileFormRedux = reduxForm({form: 'profileEdit'})(ProfileForm)
+export default ProfileFormRedux
