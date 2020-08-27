0c2f300e5064b98abb9fb7b5e39acda4f4e93c63
RavilAxmadyllin <craftwaymiks@gmail.com>
2020-08-27 12:36:35 +0300

    Updates

---
+++ precache-manifest.ea316c94bd9800986f868f8b1f50b77a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985d023e721f1a0d54d32adf14863038",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "3b60a32c4adc6d52f7d4",
+    "url": "/social-network/static/css/3.bc8c7c38.chunk.css"
+  },
+  {
+    "revision": "41dafe8d0252fbdefbfc",
+    "url": "/social-network/static/css/main.40af98e8.chunk.css"
+  },
+  {
+    "revision": "51ef2f769f2ede7066e6",
+    "url": "/social-network/static/js/2.9d8cc4da.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/social-network/static/js/2.9d8cc4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b60a32c4adc6d52f7d4",
+    "url": "/social-network/static/js/3.0cd72a23.chunk.js"
+  },
+  {
+    "revision": "41dafe8d0252fbdefbfc",
+    "url": "/social-network/static/js/main.4579e198.chunk.js"
+  },
+  {
+    "revision": "9c027809fc8362aafa44",
+    "url": "/social-network/static/js/runtime-main.d30acb3e.js"
+  },
+  {
+    "revision": "f4f3dd7acfd64b16aa51027159a6f5e8",
+    "url": "/social-network/static/media/user.f4f3dd7a.png"
+  }
+]);
